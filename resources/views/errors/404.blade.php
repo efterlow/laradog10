d2ee1d1127a34778390d205a726b289795e6d187
efterlow <efter.low@sinchew.com.my>
2023-11-22 08:55:17 +0000

    Initial commit from PHPSandbox

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,25 @@
+<center>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+
+Sin Chew Media Invalid access
+<br/>
+<br/>
+
+<br/>
+<br/>
+
+<br/>
+
+<br/>
+<br/>
+<br/>
+
+
+<img src="https://media.tenor.com/IHdlTRsmcS4AAAAM/404.gif" />
+<center>
